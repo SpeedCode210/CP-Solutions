0f214ea4253385cd15025f5e56b9dafca2556e7a
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2025-02-17 20:44:20 +0100

    Created 'Valid Anagram' by A2SV Hub

---
+++ 17-Feb-2025/Valid Anagram 269787.py
@@ -0,0 +1,5 @@
+# Problem: Valid Anagram - https://leetcode.com/problems/valid-anagram/description/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted([c for c in s]) == sorted([c for c in t])
