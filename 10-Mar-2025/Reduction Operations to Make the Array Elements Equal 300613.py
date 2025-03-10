3768443be70d8312333c8100f0c2fe9ae9c90099
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2025-03-10 18:24:40 +0100

    Created 'Reduction Operations to Make the Array Elements Equal' by A2SV Hub

---
+++ 10-Mar-2025/Reduction Operations to Make the Array Elements Equal 300613.py
@@ -0,0 +1,15 @@
+# Problem: Reduction Operations to Make the Array Elements Equal - https://leetcode.com/problems/reduction-operations-to-make-the-array-elements-equal/
+
+from collections import Counter
+class Solution:
+    def reductionOperations(self, nums: List[int]) -> int:
+        C = Counter(nums)
+        X = sorted([(i, C[i]) for i in C], key = lambda x: -x[0])
+        U = [0]
+        for i in range(0, len(X)):
+            U.append(U[-1] + X[i][1])
+        print(U)
+
+        if len(U) == 2:
+            return 0
+        return sum([x for x in U]) - U[-1]
