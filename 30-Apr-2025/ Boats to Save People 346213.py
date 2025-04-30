5e2bd6334807cf18079320425784bac74f1f48e0
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2025-05-01 00:16:06 +0100

    Created ' Boats to Save People' by A2SV Hub

---
+++ 30-Apr-2025/ Boats to Save People 346213.py
@@ -0,0 +1,22 @@
+# Problem:  Boats to Save People - https://leetcode.com/problems/boats-to-save-people/description/
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        if(len(people) == 1):
+            return 1
+
+        people.sort()
+        p0 = 0
+        p1 = len(people)-1
+        res = 0
+        while p0 < p1:
+            if people[p1] + people[p0] <= limit:
+                res += 1
+                p0 += 1
+                p1 -= 1
+            else:
+                res += 1
+                p1 -= 1
+        if p0 == p1:
+            res += 1
+        return res
