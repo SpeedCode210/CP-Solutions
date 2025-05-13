edbcbe19c088868612731079d9763920c37a79f2
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2025-05-13 21:17:09 +0100

    Created 'Same Tree' by A2SV Hub

---
+++ 13-May-2025/Same Tree 354919.py
@@ -0,0 +1,13 @@
+# Problem: Same Tree - https://leetcode.com/problems/same-tree/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root == None:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right))+1
