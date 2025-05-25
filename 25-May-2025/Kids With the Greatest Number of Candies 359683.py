3ed859a51517ac5a2b0005c5c12941c70d6a9573
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2025-05-25 17:15:44 +0100

    Created 'Kids With the Greatest Number of Candies' by A2SV Hub

---
+++ 25-May-2025/Kids With the Greatest Number of Candies 359683.py
@@ -0,0 +1,6 @@
+# Problem: Kids With the Greatest Number of Candies - https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        m = max(candies)
+        return [extraCandies+c >= m for c  in candies]
