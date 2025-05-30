c09cd04f84635909350641e90ab6032973da755b
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2025-05-30 18:40:16 +0100

    Created 'Ehab Is an Odd Person' by A2SV Hub

---
+++ 30-May-2025/Ehab Is an Odd Person 361263.py
@@ -0,0 +1,9 @@
+# Problem: Ehab Is an Odd Person - https://codeforces.com/problemset/problem/1174/B
+
+n = input()
+a = [int(x) for x in input().split()]
+ 
+if len(a) == len([c for c in a if c%2 == 0]) or len([c for c in a if c%2 == 0]) == 0:
+    print(*a)
+else:
+    print(*sorted(a))
