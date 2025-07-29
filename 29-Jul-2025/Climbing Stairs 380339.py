8f3e50bb762dcc418613eb15f72e58f3a7a1a563
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2025-07-29 19:43:52 +0100

    Created 'Climbing Stairs' by A2SV Hub

---
+++ 29-Jul-2025/Climbing Stairs 380339.py
@@ -0,0 +1,12 @@
+# Problem: Climbing Stairs - https://leetcode.com/problems/climbing-stairs/
+
+class Solution:
+    def __init__(self):
+        self.dp = dict()
+    def climbStairs(self, n: int) -> int:
+        if n <= 1:
+            return 1
+        if n in self.dp:
+            return self.dp[n]
+        self.dp[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        return self.dp[n]
