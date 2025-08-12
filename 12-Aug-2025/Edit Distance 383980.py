22981ead12844563fd70d61d3138ea1e86e2120f
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2025-08-12 19:21:02 +0100

    Created 'Edit Distance' by A2SV Hub

---
+++ 12-Aug-2025/Edit Distance 383980.py
@@ -0,0 +1,21 @@
+# Problem: Edit Distance - https://leetcode.com/problems/edit-distance/description/
+
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+
+        f = [[0 for i in range(len(word1)+1)] for j in range(len(word2)+1)]
+        for j in range(len(word1)+1):
+            f[0][j] = j
+        for i in range(1, len(word2)+1):
+            f[i][0] = i
+        
+        for i in range(1, len(word2)+1):
+            for j in range(1, len(word1)+1):
+                f[i][j] = f[i-1][j] + 1
+                for u in range(j-1, -1, -1):
+                    if word1[u] == word2[i-1]:
+                        f[i][j] = min(f[i][j], f[i-1][u] + j-u -1)
+                        break
+                    f[i][j] = min(f[i][j], f[i-1][u] + j-u)
+
+        return f[-1][-1]
