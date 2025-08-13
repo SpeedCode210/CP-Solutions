8adc9a4a3fef3ac2790c4d0c5c4cd25cf1660025
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2025-08-13 01:14:08 +0100

    Created 'Continuous Subarray Sum' by A2SV Hub

---
+++ 13-Aug-2025/Continuous Subarray Sum 384456.py
@@ -0,0 +1,13 @@
+# Problem: Continuous Subarray Sum - https://leetcode.com/problems/continuous-subarray-sum/
+
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        a = set([0])
+        beforeLast = nums[0]%k
+        for i in range(1, len(nums)):
+            if (beforeLast + nums[i])%k in a:
+                return True
+            a.add(beforeLast)
+            beforeLast = (beforeLast + nums[i])%k
+        return False
+        
